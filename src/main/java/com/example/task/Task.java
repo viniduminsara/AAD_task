850a6754cf17a487018972f3d6c785aed9d58170
Vin_Minz05 <125631607+viniduminsara@users.noreply.github.com>
2024-02-03 10:57:30 +0530

    Initial commit

---
+++ src/main/java/com/example/task/Task.java
@@ -0,0 +1,21 @@
+package com.example.task;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet(name = "task", urlPatterns = "/task", loadOnStartup = 5)
+public class Task extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String header1 = req.getHeader("Task-Custom-Header");
+        System.out.println(header1);
+
+        String name = req.getQueryString("name");
+    }
+}
